b0cab92c09cdd6515e786544b30a3dfc80a5e6e2
Medhavi Agrawal <medhawi09@gmail.com>
2026-04-04 00:29:44 +0530

    Basic layout created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1 className="logo">GoTrip</h1>
+      <ul className="nav-links">
+        <li>Home</li>
+        <li>Destinations</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
